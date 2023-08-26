4e0b53836aa7eb22fa38836ef0fdf13dda4aaf40
oursrabbit <giddyyang@gmail.com>
2023-08-26 14:01:45 +0800

    123
    213

--- LCAPI/Program.cs
+++ LCAPI/Program.cs
@@ -104,39 +104,7 @@ namespace LCAPI
                 {
                     Version = "v4",
                     Title = "LC API",
-                    Description = "LC网站数据库查询接口描述\r\n\r\n" +
-                    "v4\r\n\r\n" +
-                    "---Update---\r\n\r\n" +
-                    "MediaInfo.resource_file_size --> MediaInfo.resource_file_size_string\r\n\r\n" +
-                    "新增MediaInfo.video_equipment_tag\r\n\r\n" +
-                    "新增SearchJSON.video_equipment_tag\r\n\r\n" +
-                    "MediaInfo的普通查询和高级查询中，增加了SearchJSON.video_equipment_tag字段\r\n\r\n" +
-                    "v3\r\n\r\n" +
-                    "增加四种新的资源\r\n\r\n" +
-                    "Model：模型资源。DB模型类：ModelInfo，JSON类：ModelInfoJSON\r\n\r\n" +
-                    "AE：AE资源。DB模型类：AEInfo，JSON类：AEInfoJSON\r\n\r\n" +
-                    "IMG：图片资源。DB模型类：IMGInfo，JSON类：IMGInfoJSON\r\n\r\n" +
-                    "DOC：文档资源。DB模型类：DOCInfo，JSON类：DOCInfoJSON\r\n\r\n" +
-                    "---Update---\r\n\r\n" +
-                    "修复了一些之前没有验证Header的问题\r\n\r\n" +
-                    "v2\r\n\r\n" +
-                    "根据是否传输数据至服务器，只保留了GET POST\r\n\r\n" +
-                    "返回的StatusCode不为200时，使用RestResultJSON描述错误信息\r\n\r\n" +
-                    "第一次接口对接问题.txt\r\n\r\n" +
-                    "直接导出Excel，对列的编辑在Excel中进行~~~\r\n\r\n" +
-                    "云存储收费问题，视频文件上传目前都是云后台直接操作，不需要接口\r\n\r\n" +
-                    "---Update---\r\n\r\n" +
-                    "重命名了Schema，为XXXXXJSON\r\n\r\n" +
-                    "简化HTTP VERB，使用RestResultJSON返回更加详细的信息\r\n\r\n" +
-                    "重命名了API URL与parameter的名称\r\n\r\n" +
-                    "增加MediaInfoJSON.video_url与MediaInfo.video_download_url\r\n\r\n" +
-                    "查询List类型数据时，增加了page, page_size, total三个属性\r\n\r\n" +
-                    "excel数据上传时，添加了每个单元格的异常处理，新增一个URL参数\r\n\r\n" +
-                    "增加了一个单个上传MediaInfo的API\r\n\r\n" +
-                    "v1\r\n\r\n" +
-                    "UserInfo、MediaInfo、ObjectId为后端使用的MongoDB Model\r\n\r\n" +
-                    "UserInfoJSON、MediaInfoJSON、FullTextSearch、Search为前后端通讯用的JSON结构\r\n\r\n" +
-                    "UserInfo为用户信息，MediaInfo为视频资源信息\r\n\r\n",
+                    Description =  @"<a href='https://github.com/oursrabbit/LC'>LC网站数据库查询接口描述</a>"
                 });
 
                 options.OperationFilter<MyHeaderFilter>();
--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# LC
- 
+#LC网站数据库查询接口描述
+
+
+## v4 1.0.4
+
+**---Update---**
+
+1. MediaInfo.resource_file_size --> MediaInfo.resource_file_size_string
+
+2. 新增MediaInfo.video_equipment_tag
+
+3. 新增SearchJSON.video_equipment_tag
+
+4. MediaInfo的普通查询和高级查询中，增加了SearchJSON.video_equipment_tag字段
+
+## v3 1.0.3
+
+*增加四种新的资源
+
+Model：模型资源。DB模型类：ModelInfo，JSON类：ModelInfoJSON
+
+AE：AE资源。DB模型类：AEInfo，JSON类：AEInfoJSON
+
+IMG：图片资源。DB模型类：IMGInfo，JSON类：IMGInfoJSON
+
+DOC：文档资源。DB模型类：DOCInfo，JSON类：DOCInfoJSON*
+
+**---Update---**
+
+修复了一些之前没有验证Header的问题
+
+v2
+
+根据是否传输数据至服务器，只保留了GET POST
+
+返回的StatusCode不为200时，使用RestResultJSON描述错误信息
+
+第一次接口对接问题.txt
+
+直接导出Excel，对列的编辑在Excel中进行~~~
+
+云存储收费问题，视频文件上传目前都是云后台直接操作，不需要接口
+
+---Update---
+
+重命名了Schema，为XXXXXJSON
+
+简化HTTP VERB，使用RestResultJSON返回更加详细的信息
+
+重命名了API URL与parameter的名称
+
+增加MediaInfoJSON.video_url与MediaInfo.video_download_url
+
+查询List类型数据时，增加了page, page_size, total三个属性
+
+excel数据上传时，添加了每个单元格的异常处理，新增一个URL参数
+
+增加了一个单个上传MediaInfo的API
+
+v1
+
+UserInfo、MediaInfo、ObjectId为后端使用的MongoDB Model
+
+UserInfoJSON、MediaInfoJSON、FullTextSearch、Search为前后端通讯用的JSON结构
+
+UserInfo为用户信息，MediaInfo为视频资源信息
